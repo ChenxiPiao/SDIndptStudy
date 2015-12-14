04c55206fb6c7d1fd16486dba36da303423aed4d
ChenxiPiao <cpiao@Jakes-MacBook-Air.local>
2015-12-14 11:23:25 -0500

    WEB API&Frameworks

---
+++ RestfulApi/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using RestfulApiUploadingByEmail.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace RestfulApiUploadingByEmail.Controllers
+{
+    public class UserController : Controller
+    {
+        User[] users = new User[] {
+            new User { ID = 1, lower_user_name = "asd", user_key = "fgh"},
+            new User { ID = 1, lower_user_name = "qwe", user_key = "rty"},
+            new User { ID = 1, lower_user_name = "zxc", user_key = "vbn"}
+        };
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            return users;
+        }
+
+        public User getUserById(int id)
+        {
+            var user = users.FirstOrDefault((p) => p.ID == id);
+            if (user == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return user;
+
+        }
+    }
+}
---
+++ knockoutJs_framework/knockout-panel.js
@@ -0,0 +1,41 @@
+﻿define(['jquery', '/js/aspn/widgets/docs/documentUtils.js', 'ko'], function ($, documentUtils, ko) {
+
+    /**
+    * Initializes the Email panel
+    */
+    function initEmailPanel(container, widget) {
+            var html = [
+                '<div class="documents-widget-panel documents-widget-email-panel">',
+                    '<div class="email-header">',
+                        '<span class=email-title>Knockout Demo</span>',
+                        '<button type="button" class="dbutton done-button">Done</button>',
+                    '</div>',
+                    '<div class="email-content">',
+                    '</div>',
+                '</div>'
+            ].join('');
+
+            var $emailPanel = $(html).appendTo(container);
+
+            $emailPanel.find('.done-button').on('click', function () {
+                documentUtils.docEvent(widget, 'modeChange', { mode: 'search' });
+                return false;
+            });
+            
+            var EmailViewModel = function () {                
+            };
+            var emailViewModel = new EmailViewModel();
+
+            // bind the view model to the file uploads table
+            ko.applyBindings(emailViewModel, $emailPanel.get(0));
+
+            //documentUtils.docEvent.subscribe(widget, function (event) {
+            //    if (event.type == 'modeChange' && event.source == widget) {                    
+            //    }
+            //});
+
+            return $emailPanel;
+    };
+
+    return initEmailPanel;
+});
